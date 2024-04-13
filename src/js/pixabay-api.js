d53f10c8f4bc9d1e347a67118d84084536cce236
@NikolaenkoSvetlana <ledylana@googlemail.com>
2024-04-13 21:00:22 +0300

    step1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,30 @@
-const apiKey = '43214760-0343bbd140b24677312cd0c55';
+import axios from 'axios';
 
-export function fetchImages(keyword) {
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${keyword}&image_type=photo&orientation=horizontal&safesearch=true`;
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      return data.hits;
-    })
-    .catch(error => {
-      console.error('Error fetching images:', error);
-      return [];
-    });
+const apiKey = '43214760-0343bbd140b24677312cd0c55';
+let page = 1;
+const per_page = 15; // Кількість зображень на сторінці
+let currentKeyword = '';
+
+export async function fetchImages(keyword, pageNum) {
+  try {
+    // Перевірка чи потрібно повернутися до початкової сторінки при новому запиті
+    if (keyword !== currentKeyword) {
+      currentKeyword = keyword;
+      page = 1; // Скидаємо сторінку до початкового значення при новому пошуку
+    } else {
+      page = pageNum; // Встановлюємо поточну сторінку для пагінації
+    }
+
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${keyword}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`;
+    const response = await axios.get(url);
+
+    if (response.status !== 200) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response.data.hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    return [];
+  }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 import { fetchImages } from '../src/js/pixabay-api.js';
 import { clearGallery, createMarkup } from '../src/js/render-functions.js';
 
@@ -12,8 +13,9 @@ const lightbox = new SimpleLightbox('.gallery a', {
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const loader = document.querySelector('.loader');
+const btnLoad = document.querySelector('.js-load-more');
 
-searchForm.addEventListener('submit', event => {
+searchForm.addEventListener('submit', async event => {
   event.preventDefault();
   const keyword = searchInput.value.trim();
   if (!keyword) {
@@ -26,30 +28,63 @@ searchForm.addEventListener('submit', event => {
   }
   loader.style.display = 'block';
   clearGallery();
-  fetchImages(keyword)
-    .then(images => {
-      if (images.length === 0) {
-        iziToast.info({
-          position: 'topRight',
-          title: 'Info',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      } else {
-        createMarkup(images);
-        lightbox.refresh();
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching images:', error);
-      iziToast.error({
+  try {
+    const images = await fetchImages(keyword, 1); // Починаємо з першої сторінки
+    if (images.length === 0) {
+      iziToast.info({
         position: 'topRight',
-        title: 'Error',
+        title: 'Info',
         message:
-          'An error occurred while fetching images. Please try again later.',
+          'Sorry, there are no images matching your search query. Please try again!',
       });
-    })
-    .finally(() => {
-      loader.style.display = 'none';
+    } else {
+      createMarkup(images);
+      lightbox.refresh();
+      // Показуємо кнопку "Load more", якщо отримано зображення
+      btnLoad.classList.remove('load-more-hidden');
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    iziToast.error({
+      position: 'topRight',
+      title: 'Error',
+      message:
+        'An error occurred while fetching images. Please try again later.',
     });
+  } finally {
+    loader.style.display = 'none';
+  }
+});
+
+// Додаємо обробник події для кнопки "Load more"
+btnLoad.addEventListener('click', async () => {
+  const keyword = searchInput.value.trim();
+  if (!keyword) {
+    iziToast.error({
+      position: 'topRight',
+      title: 'Error',
+      message: 'Please enter a keyword for search.',
+    });
+    return;
+  }
+  // loader.style.display = 'block';
+  try {
+    const images = await fetchImages(keyword, 2); // Починаємо з другої сторінки
+    if (images.length === 0) {
+      alert('No more images available.');
+    } else {
+      createMarkup(images);
+      lightbox.refresh();
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    iziToast.error({
+      position: 'topRight',
+      title: 'Error',
+      message:
+        'An error occurred while fetching images. Please try again later.',
+    });
+  } finally {
+    loader.style.display = 'none';
+  }
 });
